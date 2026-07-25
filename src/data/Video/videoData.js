b04e9114604e74dd8612f9218e6763bfdd7cc792
Veer Singh <veerbathla0676@gmail.com>
2026-07-25 15:31:57 +0530

    fixed videoData

--- src/data/Video/videoData.js
+++ src/data/Video/videoData.js
@@ -1,31 +1,51 @@
+import video1 from "../assets/video/video1.jpg";
+import video2 from "../assets/video/video2.jpg";
+import video3 from "../assets/video/video3.jpg";
+import video4 from "../assets/video/video4.jpg";
+import video5 from "../assets/video/video5.jpg";
+import video6 from "../assets/video/video6.jpg";
+import video7 from "../assets/video/video7.jpg";
+
 const videoData = [
   {
     id: 1,
-    thumbnail: "/images/patient1-thumb.jpg",
-    videoUrl: "https://www.instagram.com/reel/XXXXXXXXXXX/",
+    thumbnail: video1,
+    videoUrl: "  https://www.instagram.com/reel/DSnDmQyAS6J/?igsh=MTdpbjZ2eGdkdHAxdQ==",
     title: "Patient testimonial 1",
   },
   {
     id: 2,
-    thumbnail: "/images/patient2-thumb.jpg",
-    videoUrl: "https://www.instagram.com/reel/YYYYYYYYYYY/",
+    thumbnail: video2,
+    videoUrl: "https://www.instagram.com/reel/DUMxn0QgV7D/?igsh=aHk4ZzV1djYxY2lh",
     title: "Patient testimonial 2",
   },
   {
     id: 3,
-    thumbnail: "/images/patient3-thumb.jpg",
-    videoUrl: "https://www.instagram.com/reel/ZZZZZZZZZZZ/",
+    thumbnail: video3,
+    videoUrl: "   https://www.instagram.com/reel/DUk2S19gXgX/?igsh=MW1ib2lxMmczMnJnaA==",
     title: "Patient testimonial 3",
   },
   {
     id: 4,
-    thumbnail: "/images/patient4-thumb.jpg",
-    videoUrl: "https://www.instagram.com/reel/AAAAAAAAAAA/",
+    thumbnail:video4,
+    videoUrl: "https://www.facebook.com/share/r/1UVRJW1ssx/",
     title: "Patient testimonial 4",
   },
   {
     id: 5,
-    thumbnail: "/images/patient5-thumb.jpg",
+    thumbnail:video5,
+    videoUrl: "https://www.instagram.com/reel/DVC5lxKga8l/?igsh=MThoanhnOHcxbWlmcA==",
+    title: "Patient testimonial 5",
+  },
+  {
+    id: 6,
+    thumbnail:video6,
+    videoUrl: "  https://www.instagram.com/reel/DXRdjneB2S8/?igsh=MXBhMHRnb2Z1OTZ0Zw==",
+    title: "Patient testimonial 5",
+  },
+  {
+    id: 7,
+    thumbnail:video7,
     videoUrl: "https://www.instagram.com/reel/BBBBBBBBBBB/",
     title: "Patient testimonial 5",
   },
