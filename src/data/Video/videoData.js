4cf4821e154f98de729f7f3cc6d1ea163de6aa33
Veer Singh <veerbathla0676@gmail.com>
2026-07-25 15:22:08 +0530

    videoData.js

---
+++ src/data/Video/videoData.js
@@ -0,0 +1,34 @@
+const videoData = [
+  {
+    id: 1,
+    thumbnail: "/images/patient1-thumb.jpg",
+    videoUrl: "https://www.instagram.com/reel/XXXXXXXXXXX/",
+    title: "Patient testimonial 1",
+  },
+  {
+    id: 2,
+    thumbnail: "/images/patient2-thumb.jpg",
+    videoUrl: "https://www.instagram.com/reel/YYYYYYYYYYY/",
+    title: "Patient testimonial 2",
+  },
+  {
+    id: 3,
+    thumbnail: "/images/patient3-thumb.jpg",
+    videoUrl: "https://www.instagram.com/reel/ZZZZZZZZZZZ/",
+    title: "Patient testimonial 3",
+  },
+  {
+    id: 4,
+    thumbnail: "/images/patient4-thumb.jpg",
+    videoUrl: "https://www.instagram.com/reel/AAAAAAAAAAA/",
+    title: "Patient testimonial 4",
+  },
+  {
+    id: 5,
+    thumbnail: "/images/patient5-thumb.jpg",
+    videoUrl: "https://www.instagram.com/reel/BBBBBBBBBBB/",
+    title: "Patient testimonial 5",
+  },
+];
+ 
+export default videoData;
