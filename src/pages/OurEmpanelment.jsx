230c7899cc252e09868b0adc6863f7fd06871d27
Veer Singh <veerbathla0676@gmail.com>
2026-07-30 14:51:24 +0530

    fixed imports

--- src/pages/OurEmpanelment.jsx
+++ src/pages/OurEmpanelment.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-import EmpanelmentHero from "../components/empanelment/EmpanelmentHero";
-import AboutEmpanelment from "../components/empanelment/AboutEmpanelment";
-import CashlessHighlight from "../components/empanelment/CashlessHighlight";
-import InsuranceGrid from "../components/empanelment/InsuranceGrid";
-import WhyChoose from "../components/empanelment/WhyChoose";
-import CashlessProcess from "../components/empanelment/CashlessProcess";
-import EmpanelmentCTA from "../components/empanelment/EmpanelmentCTA";
+import EmpanelmentHero from "../components/Empanelment/EmpanelmentHero";
+import AboutEmpanelment from "../components/Empanelment/AboutEmpanelment";
+import CashlessHighlight from "../components/Empanelment/CashlessHighlight";
+import InsuranceGrid from "../components/Empanelment/InsuranceGrid";
+import WhyChoose from "../components/Empanelment/WhyChoose";
+import CashlessProcess from "../components/Empanelment/CashlessProcess";
+import EmpanelmentCTA from "../components/Empanelment/EmpanelmentCTA";
 
 
 const OurEmpanelment = () => {
