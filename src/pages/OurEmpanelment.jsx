3c497121df1c59287c2572a1b0a1e14f84f093ed
Veer Singh <veerbathla0676@gmail.com>
2026-07-30 13:17:53 +0530

    emplament page

---
+++ src/pages/OurEmpanelment.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import EmpanelmentHero from "../components/empanelment/EmpanelmentHero";
+import AboutEmpanelment from "../components/empanelment/AboutEmpanelment";
+import CashlessHighlight from "../components/empanelment/CashlessHighlight";
+import InsuranceGrid from "../components/empanelment/InsuranceGrid";
+import WhyChoose from "../components/empanelment/WhyChoose";
+import CashlessProcess from "../components/empanelment/CashlessProcess";
+import EmpanelmentCTA from "../components/empanelment/EmpanelmentCTA";
+
+const OurEmpanelment = () => {
+  return (
+    <main className="bg-white">
+
+      <EmpanelmentHero />
+
+      <AboutEmpanelment />
+
+      <CashlessHighlight />
+
+      <InsuranceGrid />
+
+      <WhyChoose />
+
+      <CashlessProcess />
+
+      <EmpanelmentCTA />
+
+    </main>
+  );
+};
+
+export default OurEmpanelment;
