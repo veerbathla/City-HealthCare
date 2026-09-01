c1e038b904d0bb65ed7a708c3cee551f55d6c920
Shineweb2108 <ssamismk1@gmail.com>
2026-09-01 22:24:51 +0530

    Fix key casing for Privacy Policy in Navbar

--- src/components/Navbar/MainNavbar.jsx
+++ src/components/Navbar/MainNavbar.jsx
@@ -28,7 +28,7 @@ const MainNavbar = () => {
     { key: "doctors", path: "/doctors" },
     { key: "empanelment", path: "/empanelment" },
     { key: "appointment", path: "/appointment" },
-     { key: "Privacy Policy", path: "/Privacypolicy" },
+     { key: "Privacypolicy", path: "/Privacypolicy" },
     // { key: "blog", path: "/blog" },
     { key: "contact", path: "/contact" },
   ];
